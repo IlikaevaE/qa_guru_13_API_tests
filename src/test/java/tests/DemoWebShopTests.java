829562f7c959f0c34b62663bc965127e0d3d4a8a
PC <gud_135win@mail.ru>
2022-08-24 15:50:57 +0300

    updated

--- src/test/java/tests/DemoWebShopTests.java
+++ src/test/java/tests/DemoWebShopTests.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
@@ -8,12 +10,15 @@ import org.openqa.selenium.Cookie;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
 import static tests.TestData.*;
 
 public class DemoWebShopTests extends TestBase {
 
     @Test
-    @DisplayName("Регистрация пользователя")
+    @Feature("DemoWebShop: Регистрация нового пользователя в системе")
+    @Story("Заполнение формы регистрации нового пользователя с использованием UI")
+    @DisplayName("Проверяем возможность регистации нового пользователя")
     void registerNewUser() {
 
         registrationFormPage
@@ -31,7 +36,9 @@ public class DemoWebShopTests extends TestBase {
     }
 
     @Test
-    @DisplayName("Login API Test + change data in user profile")
+    @Feature("DemoWebShop: Изменение данных в системе API+UI")
+    @Story("Авторизация существующего пользователя с API и актуализация его профиля с UI")
+    @DisplayName("Проверка возможности изменения данных профиля у авторизованного пользователя")
     void updateUserProfile() {
 
         Cookie authCookie = authorizationPage.authUserTest();
@@ -40,11 +47,22 @@ public class DemoWebShopTests extends TestBase {
         getWebDriver().manage().addCookie(authCookie);
         open("http://demowebshop.tricentis.com");
         authorizationPage.checkUserAuthorization();
-        open("http://demowebshop.tricentis.com/customer/info");
-        $("#FirstName").setValue(FIRST_NAME);
-        $("#LastName").setValue(LAST_NAME);
-        $("[name='save-info-button']").click();
-        $("#FirstName").shouldHave(Condition.value(FIRST_NAME));
+
+        step("Открываем профиль пользователя", () -> {
+            open("http://demowebshop.tricentis.com/customer/info");
+        });
+        step("Вводим новое значение в поле ИМЯ", () -> {
+            $("#FirstName").setValue(FIRST_NAME);
+        });
+        step("Вводим новое значение в поле ФАМИЛИЯ", () -> {
+            $("#LastName").setValue(LAST_NAME);
+        });
+        step("Нажимаем на кнопку СОХРАНИТЬ", () -> {
+            $("[name='save-info-button']").click();
+        });
+        step("Проверяем, что изменения сохранились", () -> {
+            $("#FirstName").shouldHave(Condition.value(FIRST_NAME));
+        });
     }
 }
 
--- src/test/java/tests/UpdateUserProfile.java
+++ src/test/java/tests/UpdateUserProfile.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,13 +12,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static helpers.CustomApiListeners.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static tests.TestData.*;
 
 public class UpdateUserProfile extends TestBase{
 
     @Test
-    @DisplayName("Изменение пользовательских данных в профиле пользователя")
+    @Feature("DemoWebShop: Изменение данных в системе API+UI")
+    @Story("Изменение пользовательских данных в профиле пользователя")
+    @DisplayName("Проверка возможности изменения имени и фамилии")
     void changeUserData() {
         String cookieKey = "NOPCOMMERCE.AUTH";
         String cookieValue = given()
@@ -35,11 +40,23 @@ public class UpdateUserProfile extends TestBase{
         open("");
         Cookie authCookie = new Cookie(cookieKey, cookieValue);
         getWebDriver().manage().addCookie(authCookie);
-        open("/customer/info");
-        $("#FirstName").setValue(FIRST_NAME);
-        $("#LastName").setValue(LAST_NAME);
-        $("[name='save-info-button']").click();
-        $("#FirstName").shouldHave(Condition.value(FIRST_NAME));
+
+        step("Открываем профиль пользователя", () -> {
+            open("/customer/info");
+        });
+        step("Вводим новое значение в поле ИМЯ", () -> {
+            $("#FirstName").setValue(FIRST_NAME);
+        });
+        step("Вводим новое значение в поле ФАМИЛИЯ", () -> {
+             $("#LastName").setValue(LAST_NAME);
+        });
+        step("Нажимаем на кнопку СОХРАНИТЬ", () -> {
+            $("[name='save-info-button']").click();
+        });
+        step("Проверяем, что изменения сохранились", () -> {
+            $("#FirstName").shouldHave(Condition.value(FIRST_NAME));
+        });
+
     }
 
 }
